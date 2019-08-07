07d781ccff7671e3ed214c07ba70ce972b1cc2cc
profit0124 <profit0124@gmail.com>
2019-08-07 22:40:49 +0900

    React Router Task 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent';
+import { LEADERS } from '../shared/leaders';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -14,6 +15,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 class Main extends Component {
 
   constructor(props) {
+    
     super(props);
 
     this.state = {
@@ -21,6 +23,7 @@ class Main extends Component {
       comments: COMMENTS,
       promotions: PROMOTIONS,
       leaders: LEADERS
+      
     };
   }
 
@@ -43,16 +46,15 @@ class Main extends Component {
             comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
-
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route path='/menu/:dishId' component={DishWithId} />
-          <Route exat path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route exact path='/contactus' component={Contact} />} />
-          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
